5c77c1ee63eaf2e7fe92339206c419c755ece645
sofia-coder <79570815+sofia-coder@users.noreply.github.com>
2021-02-26 09:55:06 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <body>
 	<head><title>welcome to group 3's webpage!</title></head>
 <p align = "left">
+	
 <body background="bgpic.jpg">
 <img src = "logo.png" width = 60px height = 60px>	
 <font color = "white" size = "5" face = "arial black">
@@ -14,11 +15,13 @@ TODAY AND TOMORROW</font>
 <br>Tomorrow's destiny becomes today's direction.<br>
 <font size=4>
 <i>-John C. Maxwell</i><br>
+	
 <br>	
 <p align = "center">
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <font color = "yellow" size = "6" face = "arial black">
 TODAY'S ISSUE: PHILIPPINES' TRAGEDY OF PANDEMIC MILITARISM</font><br>
+	
 <br><font color = "white" size = "3" face = "arial">
 Series of incompetent responses were executed by the government and their continuous deprioritization leads the state to rapidly increase the Coronavirus cases in the Philippines. As the arising of this worldwide pandemic crisis, the Philippine government makes one of the worst examples of handling the pandemic due to its inefficiency married with militarism threaded throughout its responses. It is a bleak one marred by a totalitarian streak and the people’s increased realization that the state is more intent on filling jails. The global crisis is first and foremost a public health issue. Nonetheless, Philippine’s President Rodrigo Duterte has faced the Coronavirus pandemic in a decidedly militaristic fashion. The President also had a message for anyone who intended to repeat the action of San Roque residents (leftists): “I will not hesitate. My orders are to the police and military, as well as village officials if there are any trouble, or occasions where there’s violence and your lives are in danger: shoot them dead.”</font> 
 <p align = "center">
@@ -31,12 +34,14 @@ If our country’s government will be more solicitous and mindful hereafter, the
 <font color = "white" size = "3" face = "arial">
 In summary, the government should know their priorities and be aware of their incompetence in responding to the prior mentioned crisis. The battle against COVID-19 is not just a battle for the government of the Philippines yet a collective fight for every Filipino. Thus, the cooperation, relationship, and discipline of every Filipino, as well as the government in respecting and following the mandated precautionary measures prescribed by the authorities along with focusing on the primary issue than of militarism, are the most significant in determining factors in the country’s battle against COVID-19.</font>
 <br><br><br><br>
+	
 <img src = "militarism.jpg" width = 640px height = 426.5px>
 <br>
 <p align = "center">
 <font color = "yellow" size = "2" face = "arial">
 Militarization of Government? 'Correct' says Duterte.</font>
 <br><br><br><br><br><br><br><br>
+	
 <p align = left>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <font size = "2" color = "White" face="tahoma">
