d6bad8d784b96a17b756e04aa1684e0887b50998
sofia-coder <79570815+sofia-coder@users.noreply.github.com>
2021-02-26 10:43:24 -0800

    Update index.php

--- index.php
+++ index.php
@@ -21,15 +21,13 @@ TODAY AND TOMORROW</font>
 <font color = "yellow" size = "6" face = "arial black">
 TODAY'S ISSUE: PHILIPPINES' TRAGEDY OF PANDEMIC MILITARISM</font><br><br>
 	
-<font color = "white" size = "3" face = "arial">
+<font color = "white" size = "3" face = "tahoma">
 Series of incompetent responses were executed by the government and their continuous deprioritization leads the state to rapidly increase the Coronavirus cases in the Philippines. As the arising of this worldwide pandemic crisis, the Philippine government makes one of the worst examples of handling the pandemic due to its inefficiency married with militarism threaded throughout its responses. It is a bleak one marred by a totalitarian streak and the people’s increased realization that the state is more intent on filling jails. The global crisis is first and foremost a public health issue. Nonetheless, Philippine’s President Rodrigo Duterte has faced the Coronavirus pandemic in a decidedly militaristic fashion. The President also had a message for anyone who intended to repeat the action of San Roque residents (leftists): “I will not hesitate. My orders are to the police and military, as well as village officials if there are any trouble, or occasions where there’s violence and your lives are in danger: shoot them dead.” 
 <br><br>
 In the time when we should be saving lives, people are being killed either by incompetence or by the bullet. In a country already heavily afflicted with the COVID-19 pandemic, this senseless killings shows the inconsiderate of the Duterte administration and his failed leadership to solve the root of this problem which is the poverty, inequality of the people in our nation, and misplaced priorities of our government spending resources to hunt down activists while thousands of our countrymen are dying because of hunger or the virus. Hence, they should have minimized their militarism on handling this pandemic because people will only be more afraid than they are now. As a matter of fact, there are alternatives and programs to discipline people without being militaristic to the extent of killing and shaming violators.
 <br><br>
-<font color = "white" size = "3" face = "arial">
-If our country’s government will be more solicitous and mindful hereafter, the Philippines will be more stable in terms of environment and economic growth and will rapidly decrease the cases of COVID-19 supposing that the Government’s administration and management of the pandemic will not prioritize the subject matter, militarism. Additionally, if the nation's government implement the less militaristic response to this crisis, it will appease and will grant the community's protection than them being threatened by the government's manipulation with the pandemic.</font>
+If our country’s government will be more solicitous and mindful hereafter, the Philippines will be more stable in terms of environment and economic growth and will rapidly decrease the cases of COVID-19 supposing that the Government’s administration and management of the pandemic will not prioritize the subject matter, militarism. Additionally, if the nation's government implement the less militaristic response to this crisis, it will appease and will grant the community's protection than them being threatened by the government's manipulation with the pandemic.
 <br><br>
-<font color = "white" size = "3" face = "arial">
 In summary, the government should know their priorities and be aware of their incompetence in responding to the prior mentioned crisis. The battle against COVID-19 is not just a battle for the government of the Philippines yet a collective fight for every Filipino. Thus, the cooperation, relationship, and discipline of every Filipino, as well as the government in respecting and following the mandated precautionary measures prescribed by the authorities along with focusing on the primary issue than of militarism, are the most significant in determining factors in the country’s battle against COVID-19.</font>
 <br><br><br><br>
 	
@@ -38,10 +36,10 @@ In summary, the government should know their priorities and be aware of their in
 <p align = "center">
 <font color = "yellow" size = "2" face = "arial">
 Militarization of Government? 'Correct' says Duterte.</font>
-<br><br><br><br><br>
+<br><br><br><br>
 
 <p align = "center">
-<br><br><br><br><br>
+<br><br><br><br>
 <font color = "yellow" size = "6" face = "arial black">
 TOMORROW'S SUBJECT: AN IMAGE OF HOPE</font>
 <br><br>
@@ -56,30 +54,42 @@ As another year begins, it is important to be grateful that with all the things 
 <img src = "anchor.jpg" width = 384px height = 384px>
 <br>
 <p align = "center">
-<font color = "yellow" size = "3" face = "arial">
-The anchor is a Symbol of hope and steadfastness. 
-<br><br>
+<font color = "yellow" size = "3" face = "Tahoma">
+The anchor is a Symbol of hope and steadfastness.<br>
 In ancient times, the anchor was a symbol of safety. Christ is the anchor of Christianity and he preached hope to people. According to the Epistle to the Hebrews “Hope... is an anchor of the soul, sure and firm” (Hebrews 6:19-20). The anchor holds Christians steady to the hope of eternal salvation. Everytime the anchor rises, a new voyage will begin. They will explore and enjoy another journey!</font>
 <br><br><br><br>
 	
 <p align = "center">
 <br><br><br><br><br>
-<font color = "yellow" size = "6" face = "arial black">
 <img src = "messages of hope.jpg" width = 800px height = 282px>
 <br><br>
-<font color = "white" size = "3" face = "arial">
-“He will wipe away every tear from their eyes, and death shall be no more, neither shall there be mourning, nor crying, nor pain anymore, for the former things have passed away.” 
-<br>
+<font color = "white" size = "3" face = "Tahoma">
+“He will wipe away every tear from their eyes, and death shall be no more, 
+<br> neither shall there be mourning, nor crying, 
+<br> nor pain anymore, for the former things have passed away.” 
+<br><br>
 (Revelation 21:4)
 <br><br>
-“Have no anxiety at all; but in everything, by prayer and petition, with thanksgiving, make your requests known to God.”
-<br>
+“Have no anxiety at all; but in everything, 
+<br> by prayer and petition, with thanksgiving, 
+<br> make your requests known to God.”
+<br><br>
 (Philippians 4:6)</font>
 <br><br>
 	
+<p align = "left">
 <img src = "miss_alicia.jpg" width = 380px height = 352px>
 <p align = "right">
-Ms. Alicia P. Catabay once said that; “At the end of the day all we need is hope and strength. Hope that it will eventually get better and strength to hold on till it does”. And while we are waiting; whatever hardship, challenges, indecisions, and fears that we are facing, we just remember that these too shall pass and by the grace of God we will overcome. You might have lost someone you treasure so much, or you might be struggling to survive or to live, however all of those will be part of paragraphs in a book filled with your own journey. This means the end of that page is just there somewhere, so do not lose faith and keep hoping no matter what. Also, always remember that no matter where you are, who you are and what your situation is, may our powerful God grant you courage and strength. May you never give up because it may be hard today, but no one knows what tommorrow holds. </font>
+Ms. Alicia P. Catabay once said that; “At the end of the day all we need is hope and strength.<br>
+Hope that it will eventually get better and strength to hold on till it does”.<br>
+And while we are waiting; whatever hardship, challenges, indecisions, and fears that we are facing,<br>
+we just remember that these too shall pass and by the grace of God we will overcome.<br>
+You might have lost someone you treasure so much, or you might be struggling to survive or to live,<br>
+however all of those will be part of paragraphs in a book filled with your own journey.<br>
+This means the end of that page is just there somewhere, so do not lose faith and keep hoping no matter what.<br>
+Also, always remember that no matter where you are, who you are and what your situation is,<br>
+may our powerful God grant you courage and strength. May you never give up because it may be hard today,<br>
+but no one knows what tommorrow holds. </font>
 <br><br><br><br>
 	
 <p align = left>
