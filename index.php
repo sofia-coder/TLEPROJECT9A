339638d837440f0a7970abe320f7d44d5736bfd8
sofia-coder <79570815+sofia-coder@users.noreply.github.com>
2021-02-26 10:13:47 -0800

    Update index.php

--- index.php
+++ index.php
@@ -16,21 +16,19 @@ TODAY AND TOMORROW</font>
 <font size=4>
 <i>-John C. Maxwell</i><br>
 	
-<br>	
 <p align = "center">
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <font color = "yellow" size = "6" face = "arial black">
 TODAY'S ISSUE: PHILIPPINES' TRAGEDY OF PANDEMIC MILITARISM</font><br>
 	
-<br><font color = "white" size = "3" face = "arial">
-Series of incompetent responses were executed by the government and their continuous deprioritization leads the state to rapidly increase the Coronavirus cases in the Philippines. As the arising of this worldwide pandemic crisis, the Philippine government makes one of the worst examples of handling the pandemic due to its inefficiency married with militarism threaded throughout its responses. It is a bleak one marred by a totalitarian streak and the people’s increased realization that the state is more intent on filling jails. The global crisis is first and foremost a public health issue. Nonetheless, Philippine’s President Rodrigo Duterte has faced the Coronavirus pandemic in a decidedly militaristic fashion. The President also had a message for anyone who intended to repeat the action of San Roque residents (leftists): “I will not hesitate. My orders are to the police and military, as well as village officials if there are any trouble, or occasions where there’s violence and your lives are in danger: shoot them dead.”</font> 
-<p align = "center">
 <font color = "white" size = "3" face = "arial">
-In the time when we should be saving lives, people are being killed either by incompetence or by the bullet. In a country already heavily afflicted with the COVID-19 pandemic, this senseless killings shows the inconsiderate of the Duterte administration and his failed leadership to solve the root of this problem which is the poverty, inequality of the people in our nation, and misplaced priorities of our government spending resources to hunt down activists while thousands of our countrymen are dying because of hunger or the virus. Hence, they should have minimized their militarism on handling this pandemic because people will only be more afraid than they are now. As a matter of fact, there are alternatives and programs to discipline people without being militaristic to the extent of killing and shaming violators.</font>
-<p align = "center">
+Series of incompetent responses were executed by the government and their continuous deprioritization leads the state to rapidly increase the Coronavirus cases in the Philippines. As the arising of this worldwide pandemic crisis, the Philippine government makes one of the worst examples of handling the pandemic due to its inefficiency married with militarism threaded throughout its responses. It is a bleak one marred by a totalitarian streak and the people’s increased realization that the state is more intent on filling jails. The global crisis is first and foremost a public health issue. Nonetheless, Philippine’s President Rodrigo Duterte has faced the Coronavirus pandemic in a decidedly militaristic fashion. The President also had a message for anyone who intended to repeat the action of San Roque residents (leftists): “I will not hesitate. My orders are to the police and military, as well as village officials if there are any trouble, or occasions where there’s violence and your lives are in danger: shoot them dead.” 
+<br><br>
+In the time when we should be saving lives, people are being killed either by incompetence or by the bullet. In a country already heavily afflicted with the COVID-19 pandemic, this senseless killings shows the inconsiderate of the Duterte administration and his failed leadership to solve the root of this problem which is the poverty, inequality of the people in our nation, and misplaced priorities of our government spending resources to hunt down activists while thousands of our countrymen are dying because of hunger or the virus. Hence, they should have minimized their militarism on handling this pandemic because people will only be more afraid than they are now. As a matter of fact, there are alternatives and programs to discipline people without being militaristic to the extent of killing and shaming violators.
+<br><br>
 <font color = "white" size = "3" face = "arial">
 If our country’s government will be more solicitous and mindful hereafter, the Philippines will be more stable in terms of environment and economic growth and will rapidly decrease the cases of COVID-19 supposing that the Government’s administration and management of the pandemic will not prioritize the subject matter, militarism. Additionally, if the nation's government implement the less militaristic response to this crisis, it will appease and will grant the community's protection than them being threatened by the government's manipulation with the pandemic.</font>
-<p align = "center">
+<br><br>
 <font color = "white" size = "3" face = "arial">
 In summary, the government should know their priorities and be aware of their incompetence in responding to the prior mentioned crisis. The battle against COVID-19 is not just a battle for the government of the Philippines yet a collective fight for every Filipino. Thus, the cooperation, relationship, and discipline of every Filipino, as well as the government in respecting and following the mandated precautionary measures prescribed by the authorities along with focusing on the primary issue than of militarism, are the most significant in determining factors in the country’s battle against COVID-19.</font>
 <br><br><br><br>
@@ -40,6 +38,28 @@ In summary, the government should know their priorities and be aware of their in
 <p align = "center">
 <font color = "yellow" size = "2" face = "arial">
 Militarization of Government? 'Correct' says Duterte.</font>
+<br><br><br><br><br><br><br><br>
+
+<p align = "center">
+<br><br><br><br><br><br><br><br><br><br>
+<font color = "yellow" size = "6" face = "arial black">
+TOMORROW'S SUBJECT: AN IMAGE OF HOPE</font>
+
+<font color = "white" size = "3" face = "arial">
+Introduction 	
+<br><br>
+The year 2020 has been one extraordinary year. It made humanity question and realize a lot of things and proved that wealth is nothing without health.
+<br><br>
+As another year begins, it is important to be grateful that with all the things that have happened in the past year, we are still here alive and continuing to survive. As a member of the Social Action Office of the City Bacolod, we would like to spread a message of hope in order to start this New Year right.
+<br><br><br><br>
+	
+<img src = "anchor.jpg" width = 384px height = 384px>
+<br>
+<p align = "center">
+<font color = "yellow" size = "3" face = "arial">
+The anchor is a Symbol of hope and steadfastness. 
+<br><br>
+In ancient times, the anchor was a symbol of safety. Christ is the anchor of Christianity and he preached hope to people. According to the Epistle to the Hebrews “Hope... is an anchor of the soul, sure and firm” (Hebrews 6:19-20). The anchor holds Christians steady to the hope of eternal salvation. Everytime the anchor rises, a new voyage will begin. They will explore and enjoy another journey!</font>
 <br><br><br><br><br><br><br><br>
 	
 <p align = left>
