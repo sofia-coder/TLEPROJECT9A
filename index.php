dd718efe5e12b2e322ee11288570e780507c53fc
sofia-coder <79570815+sofia-coder@users.noreply.github.com>
2021-02-26 09:26:26 -0800

    Update index.php

--- index.php
+++ index.php
@@ -16,18 +16,18 @@ TODAY AND TOMORROW</font>
 <i>-John C. Maxwell</i><br>
 <br>	
 <p align = "center">
-<font color = "yellow" size = "5" face = "arial black">
+<font color = "yellow" size = "7" face = "arial black">
 TODAY'S ISSUE: PHILIPPINES' TRAGEDY OF PANDEMIC MILITARISM</font><br>
-<br><font color = "yellow" size = "2.5" face = "arial">
+<br><font color = "yellow" size = "3" face = "arial">
 Series of incompetent responses were executed by the government and their continuous deprioritization leads the state to rapidly increase the Coronavirus cases in the Philippines. As the arising of this worldwide pandemic crisis, the Philippine government makes one of the worst examples of handling the pandemic due to its inefficiency married with militarism threaded throughout its responses. It is a bleak one marred by a totalitarian streak and the people’s increased realization that the state is more intent on filling jails. The global crisis is first and foremost a public health issue. Nonetheless, Philippine’s President Rodrigo Duterte has faced the Coronavirus pandemic in a decidedly militaristic fashion. The President also had a message for anyone who intended to repeat the action of San Roque residents (leftists): “I will not hesitate. My orders are to the police and military, as well as village officials if there are any trouble, or occasions where there’s violence and your lives are in danger: shoot them dead.”</font> 
 <p align = "center">
-<font color = "yellow" size = "2.5" face = "arial">
+<font color = "yellow" size = "3" face = "arial">
 In the time when we should be saving lives, people are being killed either by incompetence or by the bullet. In a country already heavily afflicted with the COVID-19 pandemic, this senseless killings shows the inconsiderate of the Duterte administration and his failed leadership to solve the root of this problem which is the poverty, inequality of the people in our nation, and misplaced priorities of our government spending resources to hunt down activists while thousands of our countrymen are dying because of hunger or the virus. Hence, they should have minimized their militarism on handling this pandemic because people will only be more afraid than they are now. As a matter of fact, there are alternatives and programs to discipline people without being militaristic to the extent of killing and shaming violators.</font>
 <p align = "center">
-<font color = "yellow" size = "2.5" face = "arial">
+<font color = "yellow" size = "3" face = "arial">
 If our country’s government will be more solicitous and mindful hereafter, the Philippines will be more stable in terms of environment and economic growth and will rapidly decrease the cases of COVID-19 supposing that the Government’s administration and management of the pandemic will not prioritize the subject matter, militarism. Additionally, if the nation's government implement the less militaristic response to this crisis, it will appease and will grant the community's protection than them being threatened by the government's manipulation with the pandemic.</font>
 <p align = "center">
-<font color = "yellow" size = "2.5" face = "arial">
+<font color = "yellow" size = "3" face = "arial">
 In summary, the government should know their priorities and be aware of their incompetence in responding to the prior mentioned crisis. The battle against COVID-19 is not just a battle for the government of the Philippines yet a collective fight for every Filipino. Thus, the cooperation, relationship, and discipline of every Filipino, as well as the government in respecting and following the mandated precautionary measures prescribed by the authorities along with focusing on the primary issue than of militarism, are the most significant in determining factors in the country’s battle against COVID-19.</font>
 	
 <p align = left>
